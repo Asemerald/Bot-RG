5b3d7fbaed1ae2beb73abeedb3bad53fd7be3124
Asemerald <tdruesne05@gmail.com>
2021-09-22 19:07:27 +0300

    add fp function/timer because draftbot is lent wsh

--- index.js
+++ index.js
@@ -21,9 +21,41 @@ client.on("message", (message) => {
         .setLabel('clique moi fort senpai')
         .setStyle('PRIMARY'),
     );
+    setTimeout(function(){
+      message.reply({ content: 'Bien ouej, mets un timer maintenant', components: [row] });
+    }, 1000);
+}
+});
 
-   message.reply({ content: 'Bien ouej, mets un timer maintenant', components: [row] });
-   
+client.on("message", (message) => {
+  if(message.content === "!fp") {
+    const row = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+        .setCustomId('1h')
+        .setLabel('1h')
+        .setStyle('PRIMARY'),
+      new MessageButton() 
+        .setCustomId('2h')
+        .setLabel('2h')
+        .setStyle('PRIMARY'),
+      new MessageButton() 
+        .setCustomId('3h')
+        .setLabel('3h')
+        .setStyle('PRIMARY'),
+      new MessageButton() 
+        .setCustomId('4h')
+        .setLabel('4h')
+        .setStyle('PRIMARY'),
+      new MessageButton()
+        .setCustomId('5h')
+        .setLabel('5h')
+        .setStyle('PRIMARY'),
+    );
+    
+    setTimeout(function(){
+      message.reply({ content: 'Bien ouej bg, mets un timer maintenant', components: [row] });
+    }, 1000);
 }
 });
 
@@ -36,8 +68,9 @@ client.on("message", (message) => {
         .setLabel('Compris !')
         .setStyle('SUCCESS'),
     );
-      message.reply({ content: `Hello, voici comment je fonctionne : \n - A chaque !r, j'enverrai un message avec en dessous des boutons indiquant des durées. Il te suffit simplement de cliquer sur la durée qui te convient pour te faire ping au bout du temps que tu a choisis \n - Je réagis aussi au !gd ! Il te suffit de cliquer sur le boutton pour te faire mentionner 22h après. \n - Pour les !fp, c'est pareil que les !r \n - Il est possible que je restart a des moments de la journée, dans ce cas, les timers mit en place seront perdu (rassurez vous c'est rare, mais je préviens au cas ou), croyez moi c'est aussi relou pour vous que pour moi \n - Si jamais vous trouvez un bug, avez une question ou une idée de feature, vous pouvez toujours venir me mp : samohT#2768 (ya des chances que je sois sur le serveur, me ping marche aussi)`, components: [row] });
-   
+    setTimeout(function(){
+      message.reply({ content: `Hello bg, voici comment je fonctionne : \n - A chaque !r, j'enverrai un message avec en dessous des boutons indiquant des durées. Il te suffit simplement de cliquer sur la durée qui te convient pour te faire ping au bout du temps que tu a choisis \n - Je réagis aussi au !gd ! Il te suffit de cliquer sur le boutton pour te faire mentionner 22h après. \n - Pour les !fp, c'est pareil que les !r \n - Il est possible que je restart a des moments de la journée, dans ce cas, les timers mit en place seront perdu (rassurez vous c'est rare, mais je préviens au cas ou), croyez moi c'est aussi relou pour vous que pour moi \n - Si jamais vous trouvez un bug, avez une question ou une idée de feature, vous pouvez toujours venir me mp : samohT#2768 (ya des chances que je sois sur le serveur, me ping marche aussi)`, components: [row] });
+    }, 0);
 }
 });
 
@@ -67,9 +100,9 @@ client.on("message", (message) => {
         .setStyle('SECONDARY'),
     );
     
-
-   message.reply({ content: 'Bien ouej, mets un timer maintenant', components: [row] });
-   
+    setTimeout(function(){
+      message.reply({ content: 'Bien ouej, mets un timer maintenant', components: [row] });
+    }, 1000);
 }
 });
 
@@ -123,6 +156,20 @@ client.on("interactionCreate", async (interaction) => {
     setTimeout(function(){
       interaction.channel.send(`${interaction.user}, l'heure du !r`);
   }, 10800000);
+  interaction.message.delete()
+  }
+  if (interaction.customId === "4h") {
+    interaction.reply("A dans 4h bg")
+    setTimeout(function(){
+      interaction.channel.send(`${interaction.user}, l'heure du !r`);
+  }, 14400000 );
+  interaction.message.delete()
+  }
+  if (interaction.customId === "5h") {
+    interaction.reply("A dans 5h bg")
+    setTimeout(function(){
+      interaction.channel.send(`${interaction.user}, l'heure du !r`);
+  }, 18000000);
   interaction.message.delete()
   }
   if (interaction.customId === "6h") {
