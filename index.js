d94f789111e6dfb5dcfe37da5f41e34453d1cea4
Asemerald <tdruesne05@gmail.com>
2021-09-22 11:30:05 +0300

    add rg bot files

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client, Intents, Interaction } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+
+client.on("message", (message) => {
+  if(message.content === "!gd") {
+    const row = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+        .setCustomId('timer')
+        .setLabel('clique moi fort senpai')
+        .setStyle('PRIMARY'),
+    );
+
+   message.reply({ content: 'Bien ouej, mets un timer maintenant', components: [row] });
+   
+}
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if(interaction.isButton()) {
+    if (interaction.customId === "timer") {
+      interaction.reply("A dans 22h bg")
+      setTimeout(function(){
+        interaction.channel.send("<@378513552270557185>, <@328585121928314881> et <@266231840237551616> aka les bg, c'est l'heure");
+     }, 79200000);
+  }};
+})
+
+
+client.login('ODkwMTI1Nzc2NjE4NDE4MTk4.YUrQOg.ypTq3pKoqEcqg1lq1WJr4tSBNOQ');
